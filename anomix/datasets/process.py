ddb7da564a5b2cccd91d73c46792fc868e1a1bfa
Patryk Filip Gryz <pfgryz@gmail.com>
2025-06-08 11:25:09 +0200

    fix processing ds

--- anomix/datasets/process.py
+++ anomix/datasets/process.py
@@ -31,14 +31,19 @@ def process_datasets(definitions: list[DatasetDefinition]) -> None:
             f"{definition.label_column} not in columns for dataset {definition.name}"
         )
 
+
         labels = 1 - 2 * (df[definition.label_column] == definition.anomalous_value).astype(int)
 
         categorical_cols = df.select_dtypes(include=["object", "category"]).columns.tolist()
         categorical_cols = [col for col in categorical_cols if col != "__label__"]
-        df_encoded = pd.get_dummies(df[categorical_cols], drop_first=False)
 
-        other_cols = df.drop(columns=categorical_cols + ["__label__"])
-        df_final = pd.concat([other_cols, df_encoded, labels], axis=1)
+        if categorical_cols != 0:
+            df_encoded = pd.get_dummies(df[categorical_cols], drop_first=False)
+            other_cols = df.drop(columns=categorical_cols + [definition.label_column])
+            df_final = pd.concat([other_cols, df_encoded, labels], axis=1)
+        else:
+            other_cols = df.drop(columns=[definition.label_column])
+            df_final = pd.concat([other_cols, labels], axis=1)
 
         definition = Dataset.from_pandas(df_final, preserve_index=False)
         definition.save_to_disk(str(output_path))
